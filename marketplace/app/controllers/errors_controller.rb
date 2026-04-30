d8085663d274d50742c000df1a5fc722572daf48
Ore <valuedcustomer@Valueds-MacBook-Pro.local>
2026-04-30 13:37:22 -0700

    ProductionRailway-4

--- marketplace/app/controllers/errors_controller.rb
+++ marketplace/app/controllers/errors_controller.rb
@@ -2,14 +2,24 @@ class ErrorsController < ApplicationController
   layout "application"
 
   def not_found
-    render status: :not_found
+    render_error(:not_found, "Not Found")
   end
 
   def unprocessable
-    render status: :unprocessable_entity
+    render_error(:unprocessable_entity, "Unprocessable Entity")
   end
 
   def internal
-    render status: :internal_server_error
+    render_error(:internal_server_error, "Internal Server Error")
+  end
+
+  private
+
+  def render_error(status, message)
+    respond_to do |format|
+      format.html { render status: status }
+      format.json { render json: { error: message }, status: status }
+      format.any { head status }
+    end
   end
 end
